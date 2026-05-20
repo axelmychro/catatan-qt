1724b241415927e9799e78c98b11a611834a80bf
Axel <axelmychro@gmail.com>
2026-05-20 20:16:02 +0700

    shell: remove web dep

--- shell.nix
+++ shell.nix
@@ -4,20 +4,17 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     qt6.qtbase
-    qt6.qtbase
-    qt6.qtwebengine
-    qt6.qttools
-    qt6.qtdeclarative
     qt6.qt5compat
-    qt6.qtwebchannel
+    qt6.qttools
     qt6.qtpositioning
+    qt6.qtdeclarative
 
-    cmake
-    gnumake
-    gcc
-    gdb
-
+    libglvnd
     qtcreator
+    gcc
+    gnumake
+    cmake
+    gdb
   ];
   shellHook = ''
     echo "Entering Qt6 development environment"
