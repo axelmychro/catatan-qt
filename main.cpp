3cbf5484a61bb908ac28e56a036cda8bd060ee40
Axel <axelmychro@gmail.com>
2026-05-30 15:27:47 +0700

    Refactor: Remove ui file

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return QCoreApplication::exec();
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
+	return QCoreApplication::exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,29 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QEvent>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+class MainWindow : public QMainWindow {
+	Q_OBJECT
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+    public:
+	explicit MainWindow(QWidget *parent = nullptr);
+	~MainWindow() override;
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-protected:
-    bool eventFilter(QObject *obj, QEvent *event) override;
-
-private slots:
-    void ubahWarnaBackground();
-    void simpanFile();
-
-private:
-    Ui::MainWindow *ui;
+    private:
 };
-
-#endif
+#endif // MAINWINDOW_H
