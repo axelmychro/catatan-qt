ad34712d737db1b258305a5ec06310e9687528bf
Axel <axelmychro@gmail.com>
2026-05-31 23:26:36 +0700

    Style: Modify

--- theme.h
+++ theme.h
@@ -15,8 +15,10 @@ inline QString load()
 	qss.replace("{{bg}}", "#24273a"); // base
 	qss.replace("{{mantle}}", "#1e2030"); // mantle
 	qss.replace("{{fg}}", "#cad3f5"); // text
+	qss.replace("{{subtext}}", "#a5adcb"); // subtext
 	qss.replace("{{muted}}", "#a5adcb"); // muted
-	qss.replace("{{border}}", "#363a4f"); // surface
+	qss.replace("{{border}}", "{{surface}}"); // surface
+	qss.replace("{{surface}}", "#363a4f"); // surface
 	qss.replace("{{hover}}", "#363a4f"); // surface
 	qss.replace("{{active}}", "#6e738d"); // overlay
 	qss.replace("{{primary}}", "#f5bde6"); // pink
