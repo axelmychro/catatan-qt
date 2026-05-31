547097c4173f5af2b94280a562e9919d3b120cdf
Axel <axelmychro@gmail.com>
2026-05-31 22:45:01 +0700

    Toolbar: Capitalize

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,17 +7,19 @@ MainWindow::MainWindow(QWidget *parent)
 	setWindowTitle("Catatan");
 	resize(1280, 720);
 
-	// TOOLBAR, TOP BAR, BUTTONS
+	// TOOLBAR, TOP BAR, BUTTON
 	QToolBar *top_bar = addToolBar("top_bar");
 	top_bar->setMovable(false);
 	top_bar->setFloatable(false);
 	top_bar->setIconSize(QSize(16, 16));
 
-	top_bar->addAction("file");
-	top_bar->addAction("undo");
-	top_bar->addAction("redo");
-	top_bar->addAction("view");
-	top_bar->addAction("window");
+	auto *fileAction = top_bar->addAction("File");
+	fileAction->setToolTip("Olah catatan");
+
+	top_bar->addAction("Undo");
+	top_bar->addAction("Redo");
+	top_bar->addAction("View");
+	top_bar->addAction("Window");
 
 	// auto *central = new QWidget(this);
 	// setCentralWidget(central);
