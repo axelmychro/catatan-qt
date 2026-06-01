93608a58f8fa01b73f6388d9f792d487e7275d2f
Axel <axelmychro@gmail.com>
2026-06-02 06:49:25 +0700

    Format file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-// main.cpp
+// begin main.cpp
 #include "mainwindow.h"
 #include <QApplication>
 #include "theme.h"
 
 int main(int argc, char *argv[])
 {
-	QApplication app(argc, argv);
+		QApplication app(argc, argv);
 
-	app.setStyleSheet(Theme::load());
+		app.setStyleSheet(Theme::load());
 
-	MainWindow window;
-	window.show();
-	return app.exec();
+		MainWindow window;
+		window.show();
+		return app.exec();
 }
+// end main.cpp
