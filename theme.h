f61f20b25f6d2dece2dc86107246aad6e79b6969
Axel <axelmychro@gmail.com>
2026-06-02 12:11:22 +0700

    Feature: Open folder

--- theme.h
+++ theme.h
@@ -6,23 +6,24 @@ namespace Theme
 {
 inline QString load()
 {
-	QFile file(":/main.qss");
-	if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-		return "";
-	}
+		QFile file(":/main.qss");
+		if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+				return "";
+		}
 
-	QString qss = QTextStream(&file).readAll();
-	qss.replace("{{bg}}", "#24273a"); // base
-	qss.replace("{{mantle}}", "#1e2030"); // mantle
-	qss.replace("{{fg}}", "#cad3f5"); // text
-	qss.replace("{{subtext}}", "#a5adcb"); // subtext
-	qss.replace("{{muted}}", "#a5adcb"); // muted
-	qss.replace("{{border}}", "{{surface}}"); // surface
-	qss.replace("{{surface}}", "#363a4f"); // surface
-	qss.replace("{{hover}}", "#363a4f"); // surface
-	qss.replace("{{active}}", "#6e738d"); // overlay
-	qss.replace("{{primary}}", "#f5bde6"); // pink
+		QString qss = QTextStream(&file).readAll();
+		qss.replace("{{bg}}", "#24273a"); // base
+		qss.replace("{{mantle}}", "#1e2030"); // mantle
+		qss.replace("{{mantle}}", "#181926"); // base
+		qss.replace("{{fg}}", "#cad3f5"); // text
+		qss.replace("{{subtext}}", "#a5adcb"); // subtext
+		qss.replace("{{muted}}", "#a5adcb"); // muted
+		qss.replace("{{border}}", "{{surface}}"); // surface
+		qss.replace("{{surface}}", "#363a4f"); // surface
+		qss.replace("{{hover}}", "#363a4f"); // surface
+		qss.replace("{{active}}", "#6e738d"); // overlay
+		qss.replace("{{primary}}", "#f5bde6"); // pink
 
-	return qss;
+		return qss;
 }
 }
