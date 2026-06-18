fc0f6295b98b23b32e4485778472a946b8e8cb00
Axel <axelmychro@gmail.com>
2026-06-18 11:55:19 +0700

    Add web engine

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
-// begin main.cpp
 #include "mainwindow.h"
 #include <QApplication>
-#include "theme.h"
 
 int main(int argc, char *argv[])
 {
 		QApplication app(argc, argv);
-
-		app.setStyleSheet(Theme::load());
-
 		MainWindow window;
 		window.show();
 		return app.exec();
 }
-// end main.cpp
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
-// start mainwindow.cpp
 #include "mainwindow.h"
+#include "mainwindow.moc"
+#include <QWebEngineView>
+#include <QWebEnginePage>
 #include <QTextEdit>
 #include <QMenu>
 #include <QToolBar>
@@ -24,7 +26,6 @@
 #include <QDateTime>
 #include <QApplication>
 
-// SUB-CLASS KHUSUS: Mengajari QTextEdit agar bisa mengenali Paste Gambar secara otomatis
 class CustomTextEdit : public QTextEdit {
 	private:
 		MainWindow *m_mainWindow;
@@ -32,12 +33,10 @@ class CustomTextEdit : public QTextEdit {
 	protected:
 		void insertFromMimeData(const QMimeData *source) override
 		{
-				// Cek apakah data yang di-paste mengandung gambar/screenshot
 				if (source && source->hasImage()) {
 						m_mainWindow->handleImagePaste();
 						return;
 				}
-				// Jika teks biasa, biarkan berjalan normal seperti biasa
 				QTextEdit::insertFromMimeData(source);
 		}
 
@@ -54,7 +53,6 @@ MainWindow::MainWindow(QWidget *parent)
 {
 		setWindowTitle("Catatan");
 		resize(1280, 720);
-
 		setupToolbar();
 		setupStatusBar();
 		showWelcome();
@@ -68,43 +66,28 @@ void MainWindow::setupToolbar()
 		top_bar->setMovable(false);
 		top_bar->setFloatable(false);
 		top_bar->setIconSize(QSize(16, 16));
-
 		auto *file_action = top_bar->addAction("File");
-		file_action->setToolTip("Olah catatan");
-
 		auto *file_button = qobject_cast<QToolButton *>(
 				top_bar->widgetForAction(file_action));
 		if (file_button) {
 				file_button->setPopupMode(QToolButton::InstantPopup);
 				setupFileMenu(file_button);
 		}
-
 		auto *undo_action = top_bar->addAction("Undo");
-		undo_action->setToolTip("Undo (Ctrl+Z)");
 		undo_action->setShortcut(QKeySequence::Undo);
 		connect(undo_action, &QAction::triggered, this, &MainWindow::undo);
-
 		auto *redo_action = top_bar->addAction("Redo");
-		redo_action->setToolTip("Redo (Ctrl+Y)");
 		redo_action->setShortcut(QKeySequence::Redo);
 		connect(redo_action, &QAction::triggered, this, &MainWindow::redo);
-
 		m_preview_action = top_bar->addAction("Preview");
-		m_preview_action->setToolTip("Preview Markdown");
 		m_preview_action->setEnabled(false);
 		connect(m_preview_action, &QAction::triggered, this,
 				&MainWindow::showPreview);
-
-		// ATTACHMENT
 		m_attach_action = top_bar->addAction("Attach Image");
-		m_attach_action->setToolTip(
-				"Insert image into document (Ctrl+Shift+I)");
 		m_attach_action->setShortcut(QKeySequence("Ctrl+Shift+I"));
 		m_attach_action->setEnabled(false);
 		connect(m_attach_action, &QAction::triggered, this,
 				&MainWindow::attachImage);
-
-		top_bar->addAction("Window");
 }
 
 void MainWindow::setupFileMenu(QToolButton *parent_button)
@@ -123,23 +106,16 @@ void MainWindow::newFile()
 {
 		m_current_path.clear();
 		checkPreviewAvailability();
-
 		if (m_text_edit) {
 				m_text_edit->clear();
 				m_text_edit->setFocus();
 				updateStatus("untitled");
-				if (m_pos_label)
-						m_pos_label->setText("1:1");
 				return;
 		}
-
 		if (m_welcome)
 				m_welcome->hide();
-
-		// MENGGUNAKAN CUSTOM TEXT EDIT YANG SUDAH KITA AJARI FITUR PASTE GAMBAR
 		m_text_edit = new CustomTextEdit(this, this);
 		m_text_edit->setFrameShape(QFrame::NoFrame);
-
 		connect(m_text_edit, &QTextEdit::cursorPositionChanged, this, [this]() {
 				if (!m_pos_label || !m_text_edit)
 						return;
@@ -148,18 +124,14 @@ void MainWindow::newFile()
 											 .arg(cursor.blockNumber() + 1)
 											 .arg(cursor.columnNumber() + 1));
 		});
-
 		auto *central = new QWidget(this);
 		auto *layout = new QVBoxLayout(central);
 		layout->setContentsMargins(0, 0, 0, 0);
 		layout->addWidget(m_text_edit);
 		setCentralWidget(central);
 		m_welcome = nullptr;
-
 		m_text_edit->setFocus();
 		updateStatus("untitled");
-		if (m_pos_label)
-				m_pos_label->setText("1:1");
 }
 
 void MainWindow::openFile()
@@ -169,16 +141,11 @@ void MainWindow::openFile()
 				"Text Files (*.txt *.md);;All Files (*)");
 		if (path.isEmpty())
 				return;
-
 		QFile file(path);
-		if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-				QMessageBox::warning(this, "Error", "Cannot open:\n" + path);
+		if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
 				return;
-		}
-
 		if (!m_text_edit)
 				newFile();
-
 		m_text_edit->setPlainText(QTextStream(&file).readAll());
 		m_current_path = path;
 		checkPreviewAvailability();
@@ -191,10 +158,8 @@ void MainWindow::openFolder()
 														 QDir::homePath());
 		if (path.isEmpty())
 				return;
-
 		if (!m_folder_dock)
 				setupFolderSidebar();
-
 		m_folder_model->setRootPath(path);
 		m_folder_view->setRootIndex(m_folder_model->index(path));
 		m_folder_dock->show();
@@ -205,30 +170,16 @@ void MainWindow::setupFolderSidebar()
 {
 		m_folder_model = new QFileSystemModel(this);
 		m_folder_model->setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
-		m_folder_model->setNameFilters(QStringList()
-									   << "*.txt" << "*.md" << "*");
-		m_folder_model->setNameFilterDisables(false);
-
 		m_folder_view = new QTreeView(this);
 		m_folder_view->setModel(m_folder_model);
-		m_folder_view->setHeaderHidden(true);
-		m_folder_view->hideColumn(1);
-		m_folder_view->hideColumn(2);
-		m_folder_view->hideColumn(3);
-
 		connect(m_folder_view, &QTreeView::doubleClicked, this,
 				[this](const QModelIndex &index) {
-						if (!m_folder_model || !index.isValid())
-								return;
 						QString path = m_folder_model->filePath(index);
-						QFileInfo info(path);
-
-						if (info.isFile()) {
+						if (QFileInfo(path).isFile()) {
 								if (!m_text_edit)
 										newFile();
 								m_current_path = path;
 								checkPreviewAvailability();
-
 								QFile file(path);
 								if (file.open(QIODevice::ReadOnly |
 											  QIODevice::Text)) {
@@ -238,14 +189,8 @@ void MainWindow::setupFolderSidebar()
 								}
 						}
 				});
-
 		m_folder_dock = new QDockWidget("Files", this);
-		m_folder_dock->setObjectName("folderDock");
 		m_folder_dock->setWidget(m_folder_view);
-		m_folder_dock->setFeatures(QDockWidget::DockWidgetMovable |
-								   QDockWidget::DockWidgetClosable);
-		m_folder_dock->setAllowedAreas(Qt::LeftDockWidgetArea |
-									   Qt::RightDockWidgetArea);
 		addDockWidget(Qt::LeftDockWidgetArea, m_folder_dock);
 		m_folder_dock->hide();
 }
@@ -253,14 +198,8 @@ void MainWindow::setupFolderSidebar()
 void MainWindow::setupStatusBar()
 {
 		m_status_bar = statusBar();
-		m_status_bar->setSizeGripEnabled(false);
-
 		m_file_label = new QLabel("untitled", this);
-		m_file_label->setStyleSheet("color: {{fg}}; margin-left: 4px;");
-
 		m_pos_label = new QLabel("1:1", this);
-		m_pos_label->setStyleSheet("color: {{fg}}; margin-right: 4px;");
-
 		m_status_bar->addWidget(m_file_label, 1);
 		m_status_bar->addPermanentWidget(m_pos_label);
 }
@@ -268,50 +207,34 @@ void MainWindow::setupStatusBar()
 void MainWindow::showWelcome()
 {
 		m_text_edit = nullptr;
-
 		m_welcome = new QLabel("Catatan", this);
 		m_welcome->setAlignment(Qt::AlignCenter);
-		m_welcome->setObjectName("welcomeLabel");
-
 		setCentralWidget(m_welcome);
-
-		updateStatus("untitled");
 }
 
 void MainWindow::updateStatus(const QString &fileName)
 {
-		if (m_file_label) {
+		if (m_file_label)
 				m_file_label->setText(fileName.isEmpty() ?
 											  "untitled" :
 											  QFileInfo(fileName).fileName());
-		}
 }
 
 void MainWindow::saveFile()
 {
-		if (!m_text_edit || m_text_edit->toPlainText().isEmpty())
+		if (!m_text_edit)
 				return;
-
 		if (m_current_path.isEmpty()) {
-				QString path = QFileDialog::getSaveFileName(
+				m_current_path = QFileDialog::getSaveFileName(
 						this, "Save File", QDir::homePath(),
-						"Text Files (*.txt *.md);;All Files (*)");
-				if (path.isEmpty())
+						"Text Files (*.md)");
+				if (m_current_path.isEmpty())
 						return;
-				m_current_path = path;
 		}
-
 		QFile file(m_current_path);
-		if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-				QMessageBox::warning(this, "Error",
-									 "Cannot save:\n" + m_current_path);
-				return;
+		if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+				QTextStream(&file) << m_text_edit->toPlainText();
 		}
-
-		QTextStream out(&file);
-		out << m_text_edit->toPlainText();
-		updateStatus(m_current_path);
-		checkPreviewAvailability(); // Biar tombol langsung mendeteksi setelah save pertama kali
 }
 
 void MainWindow::closeFile()
@@ -320,42 +243,29 @@ void MainWindow::closeFile()
 		checkPreviewAvailability();
 		showWelcome();
 }
-
 void MainWindow::undo()
 {
 		if (m_text_edit)
 				m_text_edit->undo();
 }
-
 void MainWindow::redo()
 {
 		if (m_text_edit)
 				m_text_edit->redo();
 }
-
-// LOGIKA ASLI KAMU TETAP DIJAGA UTUH
 void MainWindow::checkPreviewAvailability()
 {
-		if (m_preview_action) {
-				bool isMd =
-						m_current_path.endsWith(".md", Qt::CaseInsensitive) ||
-						m_current_path.endsWith(".markdown",
-												Qt::CaseInsensitive);
+		bool isMd = m_current_path.endsWith(".md", Qt::CaseInsensitive);
+		if (m_preview_action)
 				m_preview_action->setEnabled(isMd && m_text_edit != nullptr);
-		}
-
 		if (m_attach_action)
 				m_attach_action->setEnabled(m_text_edit != nullptr);
 }
 
 void MainWindow::attachImage()
 {
-		if (!m_text_edit)
-				return;
-
 		QString src = QFileDialog::getOpenFileName(
-				this, "Attach Image", QDir::homePath(),
-				"Images (*.png *.jpg *.jpeg *.gif *.webp *.bmp *.svg);;All Files (*)");
+				this, "Attach Image", QDir::homePath(), "Images (*.png *.jpg)");
 		if (src.isEmpty())
 				return;
 
@@ -404,55 +314,19 @@ void MainWindow::attachImage()
 		m_text_edit->setTextCursor(cursor);
 }
 
-// LOGIKA PROSES SIMPAN GAMBAR DARI CLIPBOARD
 void MainWindow::handleImagePaste()
 {
-		if (!m_text_edit)
-				return;
-
 		const QClipboard *clipboard = QApplication::clipboard();
 		const QMimeData *mimeData = clipboard->mimeData();
-
 		if (mimeData->hasImage()) {
 				QImage img = qvariant_cast<QImage>(mimeData->imageData());
-				if (img.isNull())
-						return;
-
-				QString mdPath;
-				QString destPath;
 				QString fileName =
 						QString("pasted_%1.png")
 								.arg(QDateTime::currentMSecsSinceEpoch());
-
-				if (!m_current_path.isEmpty()) {
-						QFileInfo docInfo(m_current_path);
-						QDir attachDir(docInfo.dir().filePath("_attachments"));
-						if (!attachDir.exists())
-								attachDir.mkpath(".");
-
-						destPath = attachDir.filePath(fileName);
-						mdPath = QString("_attachments/%1").arg(fileName);
-				} else {
-						// Jika masih untitled, simpan di folder Temp sistem laptop
-						destPath = QDir::temp().filePath(fileName);
-						mdPath = QUrl::fromLocalFile(destPath).toString();
-				}
-
-				if (img.save(destPath, "PNG")) {
-						QString snippet =
-								QString("![Pasted Image](%2)").arg(mdPath);
-
-						QTextCursor cursor = m_text_edit->textCursor();
-						if (!cursor.atBlockStart() &&
-							cursor.block().text().length() > 0)
-								snippet.prepend("\n");
-						if (!cursor.atBlockEnd() &&
-							cursor.block().text().length() > 0)
-								snippet.append("\n");
-
-						cursor.insertText(snippet);
-						m_text_edit->setTextCursor(cursor);
-				}
+				QString destPath = QDir::temp().filePath(fileName);
+				img.save(destPath, "PNG");
+				m_text_edit->insertPlainText(
+						QString("![Pasted](%1)").arg(destPath));
 		}
 }
 
@@ -466,51 +340,47 @@ void MainWindow::showPreview()
 									  QFileInfo(m_current_path).fileName());
 		previewDialog->resize(900, 700);
 
-		QTextBrowser *browser = new QTextBrowser(previewDialog);
-		browser->setOpenExternalLinks(true);
+		QWebEngineView *webView = new QWebEngineView(previewDialog);
 
-		QFileInfo docInfo(m_current_path);
-		browser->setSearchPaths(QStringList() << docInfo.absolutePath());
+		// Convert Markdown to HTML
+		QTextDocument doc;
+		doc.setMarkdown(m_text_edit->toPlainText());
+		QString htmlContent = doc.toHtml();
+
+		// Inject CSS for resizing and constraint
+		// display: block + resize: both + overflow: auto is the standard way to enable browser-native resize
+		QString styleAndScript = R"(
+        <style>
+            body { font-family: sans-serif; color: #cad3f5; background: #24273a; padding: 20px; }
+            img { 
+                max-height: 400px; 
+                display: block;
+                resize: both;
+                overflow: auto;
+                border: 2px dashed #89b4fa;
+                padding: 2px;
+            }
+        </style>
+        <script>
+            // Optional: force images to be resizable if they don't have it by default
+            window.onload = () => {
+                document.querySelectorAll('img').forEach(img => {
+                    img.style.cursor = 'nwse-resize';
+                });
+            };
+        </script>
+    )";
+
+		// Set the base URL so images load correctly relative to the file path
+		QUrl baseUrl = QUrl::fromLocalFile(
+				QFileInfo(m_current_path).absolutePath() + "/");
+		webView->setHtml(styleAndScript + htmlContent, baseUrl);
 
 		QVBoxLayout *layout = new QVBoxLayout(previewDialog);
 		layout->setContentsMargins(0, 0, 0, 0);
-		layout->addWidget(browser);
+		layout->addWidget(webView);
 		previewDialog->setLayout(layout);
 
-		QTextDocument doc;
-		doc.setMarkdown(m_text_edit->toPlainText());
-		doc.setBaseUrl(QUrl::fromLocalFile(docInfo.absolutePath() + "/"));
-
-		QString html = doc.toHtml();
-
-		QString css =
-				"<style>"
-				"body { font-family: sans-serif; color: #cad3f5; background: #24273a; padding: 16px; line-height: 1; }"
-				"h1, h2, h3, h4, h5, h6 { color: #f5bde6; margin-top: 16px; margin-bottom: 16px; font-weight: 600; }"
-				"h1 { font-size: 2em; }"
-				"h2 { font-size: 1.5em; }"
-				"p { margin-bottom: 16px; }"
-				"code { background: #1e2030; padding: 2px 2px; border-radius: 8px; font-family: monospace; color: #a6da95; }"
-				"pre { background: #11111b; padding: 16px; border-radius: 8px; overflow-x: auto; border: 1px solid #313244; }"
-				"pre code { background: transparent; color: #cdd6f4; padding: 0; }"
-				"blockquote { border-left: 4px solid #89b4fa; margin: 16px 0; padding: 8px 16px; background: #313244; color: #a6adc8; border-radius: 0 4px 4px 0; }"
-				"ul, ol { margin-bottom: 16px; padding-left: 32px; }"
-				"li { margin-bottom: 8px; }"
-				"li input[type='checkbox'] { margin-right: 8px; transform: scale(1.2); accent-color: #89b4fa; }"
-				"hr { border: 0; border-top: 2px solid #313244; margin: 24px 0; }"
-				"a { color: #89b4fa; text-decoration: none; }"
-				"a:hover { text-decoration: underline; }"
-				"table { border-collapse: collapse; width: 100%; margin-bottom: 16px; }"
-				"th, td { border: 1px solid #313244; padding: 8px 12px; text-align: left; }"
-				"th { background: #313244; color: #89b4fa; }"
-				"strong { color: #f5c2e7; font-weight: bold; }"
-				"em { color: #a6e3a1; font-style: italic; }"
-				"del { color: #6c7086; text-decoration: line-through; }"
-				"img { max-width: 100%; border-radius: 8px; margin: 16px 0; }"
-				"</style>";
-
-		browser->setHtml(css + html);
 		previewDialog->exec();
 		previewDialog->deleteLater();
 }
-// end mainwindow.cpp
